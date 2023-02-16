833de171bdfd852ae2e0c373719ff99fae1be0d6
seththorup <seth.thorup@gmail.com>
2023-02-15 19:09:00 -0700

    Completed example

--- SeeFood/Controller/ViewController.swift
+++ SeeFood/Controller/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 
     @IBOutlet weak var photoView: UIImageView!
     
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -21,16 +23,55 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
+        
+        resultsLabel.text = "Waiting on results"
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             photoView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Error with CIImage")
+            }
+            
+            detectImage(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true)
         
+    }
+    
+    func detectImage(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Failed with VNCoreMLModel")
+        }
+        
+        let request = VNCoreMLRequest(model: model) {(request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Error with with request results")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                    
+                } else {
+                    self.navigationItem.title = "Not Hotdog..."
+                }
+                
+                self.resultsLabel.text = firstResult.identifier.description
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
 
